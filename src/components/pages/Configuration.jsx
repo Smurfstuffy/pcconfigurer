191a8b2f1ed57980ca4ed5ad74d18c34702febe4
unknown <marko.yanovych.pz.2020@lpnu.ua>
2024-05-13 03:45:58 +0300

    add configuration page styles

--- src/components/pages/Configuration.jsx
+++ src/components/pages/Configuration.jsx
@@ -1,89 +1,89 @@
 const Configuration = () => {
   return (
-    <div className="flex justify-center px-10 md:px-24 mt-2 bg-gray-200">
-      <table className="table-auto w-screen border-collapse border">
+    <div className="flex justify-center px-10 md:px-24 bg-gray-200">
+      <table className="table-auto w-screen border-collapse border mt-2">
         <thead>
-          <tr>
-            <th>Component</th>
-            <th>Selection</th>
-            <th>Base Price</th>
-            <th>Your Price</th>
-            <th>Where</th>
+          <tr className="text-gray-500 text-xs md:text-sm">
+            <th className="text-left">Component</th>
+            <th className="text-left">Selection</th>
+            <th className="text-left">Base Price</th>
+            <th className="text-left">Your Price</th>
+            <th className="text-left">Where</th>
             <th></th>
           </tr>
         </thead>
         <tbody>
           <tr className='border border-y-gray-400'>
-            <td>CPU</td>
-            <td className="text-center">Ryzen 7 1700x eD</td>
-            <td className="text-center">fds</td>
-            <td className="text-center">fds</td>
-            <td className="text-center">fds</td>
-            <td className="text-center">fsd</td>
+            <td className="text-lg md:text-xl text-blue-600 font-semibold">CPU</td>
+            <td className="text-left">Ryzen 7 1700x eD</td>
+            <td className="text-left">fds</td>
+            <td className="text-left">fds</td>
+            <td className="text-left">fds</td>
+            <td className="text-left">fsd</td>
           </tr>
           <tr className='border border-y-gray-400'>
-            <td>CPU Cooler</td>
-            <td className="text-center">fasd</td>
-            <td className="text-center">safd</td>
-            <td className="text-center">asdf</td>
-            <td className="text-center">asdf</td>
-            <td className="text-center">sadfds</td>
+            <td className="text-lg md:text-xl text-blue-600 font-semibold">CPU Cooler</td>
+            <td className="text-left">fasd</td>
+            <td className="text-left">safd</td>
+            <td className="text-left">asdf</td>
+            <td className="text-left">asdf</td>
+            <td className="text-left">sadfds</td>
           </tr>
           <tr className='border border-y-gray-400'>
-            <td>Motherboard</td>
-            <td className="text-center"></td>
-            <td className="text-center"></td>
-            <td className="text-center"></td>
-            <td className="text-center"></td>
-            <td className="text-center"></td>
+            <td className="text-lg md:text-xl text-blue-600 font-semibold">Motherboard</td>
+            <td className="text-left"></td>
+            <td className="text-left"></td>
+            <td className="text-left"></td>
+            <td className="text-left"></td>
+            <td className="text-left"></td>
           </tr>
           <tr className='border border-y-gray-400'>
-            <td>Graphical Card</td>
-            <td className="text-center"></td>
-            <td className="text-center"></td>
-            <td className="text-center"></td>
-            <td className="text-center"></td>
-            <td className="text-center"></td>
+            <td className="text-lg md:text-xl text-blue-600 font-semibold">Graphical Card</td>
+            <td className="text-left"></td>
+            <td className="text-left"></td>
+            <td className="text-left"></td>
+            <td className="text-left"></td>
+            <td className="text-left"></td>
           </tr>
           <tr className='border border-y-gray-400'>
-            <td>Memory</td>
-            <td className="text-center"></td>
-            <td className="text-center"></td>
-            <td className="text-center"></td>
-            <td className="text-center"></td>
-            <td className="text-center"></td>
+            <td className="text-lg md:text-xl text-blue-600 font-semibold">Memory</td>
+            <td className="text-left"></td>
+            <td className="text-left"></td>
+            <td className="text-left"></td>
+            <td className="text-left"></td>
+            <td className="text-left"></td>
           </tr>
           <tr className='border border-y-gray-400'>
-            <td>Storage</td>
-            <td className="text-center"></td>
-            <td className="text-center"></td>
-            <td className="text-center"></td>
-            <td className="text-center"></td>
-            <td className="text-center"></td>
+            <td className="text-lg md:text-xl text-blue-600 font-semibold">Storage</td>
+            <td className="text-left"></td>
+            <td className="text-left"></td>
+            <td className="text-left"></td>
+            <td className="text-left"></td>
+            <td className="text-left"></td>
           </tr>
           <tr className='border border-y-gray-400'>
-            <td>Power Supply</td>
-            <td className="text-center"></td>
-            <td className="text-center"></td>
-            <td className="text-center"></td>
-            <td className="text-center"></td>
-            <td className="text-center"></td>
+            <td className="text-lg md:text-xl text-blue-600 font-semibold">Power Supply</td>
+            <td className="text-left"></td>
+            <td className="text-left"></td>
+            <td className="text-left"></td>
+            <td className="text-left"></td>
+            <td className="text-left"></td>
           </tr>
           <tr className='border border-y-gray-400'>
-            <td>Case Fan</td>
-            <td className="text-center"></td>
-            <td className="text-center"></td>
-            <td className="text-center"></td>
-            <td className="text-center"></td>
-            <td className="text-center"></td>
+            <td className="text-lg md:text-xl text-blue-600 font-semibold">Case Fan</td>
+            <td className="text-left"></td>
+            <td className="text-left"></td>
+            <td className="text-left"></td>
+            <td className="text-left"></td>
+            <td className="text-left"></td>
           </tr>
           <tr className='border border-y-gray-400'>
-            <td>Case</td>
-            <td className="text-center"></td>
-            <td className="text-center"></td>
-            <td className="text-center"></td>
-            <td className="text-center"></td>
-            <td className="text-center"></td>
+            <td className="text-lg md:text-xl text-blue-600 font-semibold">Case</td>
+            <td className="text-left"></td>
+            <td className="text-left"></td>
+            <td className="text-left"></td>
+            <td className="text-left"></td>
+            <td className="text-left"></td>
           </tr>
         </tbody>
       </table>

bbb07b69e27aea2c2d9fb58c8f647801c867f495
unknown <marko.yanovych.pz.2020@lpnu.ua>
2024-05-22 05:10:39 +0300

    connect to mongodb atlas

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const userRoutes = require('./routes/user');
 const pcpartRoutes = require('./routes/pcparts');
-const partsPriceRoutes = require('./routes/partsprices')
+const partsPriceRoutes = require('./routes/partsprices');
+
+const localUri = 'mongodb://127.0.0.1:27017/pcconfigurer';
+const uri = 'mongodb+srv://diplomausername:diploma123@cluster0.gy9qitr.mongodb.net/pcconfigurer?retryWrites=true&w=majority'
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/pcconfigurer')
+mongoose.connect(uri)
   .then((result) => app.listen(8080))
   .catch((err) => console.log('failed to connect to db'))
 

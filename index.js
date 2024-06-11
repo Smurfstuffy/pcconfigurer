7235a8e8984462cdeba8b5e1fd3cded86421f575
unknown <marko.yanovych.pz.2020@lpnu.ua>
2024-06-11 19:48:08 +0300

    change db cloud uri

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ const pcpartRoutes = require('./routes/pcparts');
 const partsPriceRoutes = require('./routes/partsprices');
 
 const localUri = 'mongodb://127.0.0.1:27017/pcconfigurer';
-const uri = 'mongodb+srv://diplomausername:diploma123@cluster0.gy9qitr.mongodb.net/pcconfigurer?retryWrites=true&w=majority'
+const uri = 'mongodb+srv://diplomausername:diploma123@diplomacluster.ykf5bfo.mongodb.net/pcconfigurer?retryWrites=true&w=majority';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(localUri)
+mongoose.connect(uri)
   .then((result) => app.listen(8080))
   .catch((err) => console.log('failed to connect to db'))
 
